ebee653dabde169ba5e343381c1c5b0fc4df2617
username <drewesk@gmail.com>
2025-06-28 17:53:05 -0400

    adding color theme to app in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   content: ["./App.tsx", "./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+        light: {
+          100: '#D6C7FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+  
+        },
+        dark: {
+          100: '#221F3d',
+          200: '#0F0d23',
+        },
+        accent: '#AB8BFF',
+      },
+    },
   },
   plugins: [],
 };
