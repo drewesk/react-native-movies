8e30cd3bf5dc379739e19a15342c4ee35c77ace1
username <drewesk@gmail.com>
2025-06-30 14:23:27 -0400

    adding button functionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,32 @@
+import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { Image, ImageBackground, Text } from 'react-native'
+import { Image, ImageBackground, Text, View } from 'react-native'
+
+
+const TabIcon = ({ focused, icon, title }: any) => {
+    if (focused){
+        return (
+        <ImageBackground
+        source={images.highlight} 
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+            <Image
+                source={icon}
+                tintColor="#151312"
+                className="size-5"/>
+            <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+    
+        </ImageBackground>
+                )
+    }
+    return (
+        <View>
+            <Image source={icon} tintColor="#A8B5DB" className="size-5"/>
+        </View>
+    )
+}
 
 const _Layout = () => {
   return (
@@ -12,21 +37,11 @@ const _Layout = () => {
                 title: 'Home',
                 headerShown: false,
                 tabBarIcon: ({ focused }) => (
-                    <>
-                        <ImageBackground
-                            source={images.highlight} 
-                            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-                            >
-                            <Image
-                                source={images.home}
-                                tintColor="#151312"
-                                className="size-5"/>
-                            <Text>Home</Text>
-
-                    
-
-                        </ImageBackground>
-                    </>
+                    <TabIcon 
+                        focused={focused}
+                        icon={icons.home}
+                        title="Home"
+                        />
                 )
             }}
         />
@@ -34,7 +49,14 @@ const _Layout = () => {
             name='search'
             options={{
                 title: 'Search',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                    focused={focused}
+                    icon={icons.search}
+                    title="Search"
+                    />
+                )
             }}
         />
 
@@ -43,13 +65,27 @@ const _Layout = () => {
             options={{
                 title: 'Saved',
                 headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                    focused={focused}
+                    icon={icons.save}
+                    title="Saved"
+                    />
+                )
             }}
         />
         <Tabs.Screen
             name='profile'
             options={{
                 title: 'Profile',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon 
+                    focused={focused}
+                    icon={icons.person}
+                    title="Profile"
+                    />
+                )
             }}
         />
     </Tabs>
