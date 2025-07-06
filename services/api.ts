46c1d08ec56c8d7b2f8a1302dd7afcd69ff05a93
username <drewesk@gmail.com>
2025-07-06 14:32:40 -0400

    completed movies detail page

--- services/api.ts
+++ services/api.ts
@@ -8,22 +8,43 @@ export const TMDB_CONFIG = {
 };
 
 export const fetchMovies = async ({ query }: { query: string }) => {
-    const endpoint = query
+  const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
-    const response = await fetch(endpoint, {
-        method: 'GET',
-        headers: TMDB_CONFIG.headers,
-    });
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
 
-    if(!response.ok) {
-        throw new Error('Failed to fetch movies', response.statusText);
-    }
+  if (!response.ok) {
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const fetchMoviesDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+
+    if (!response.ok) throw new Error("Failed to fetch movie details");
 
     const data = await response.json();
 
-    return data.results;
-}
-// discover/movie
-
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
