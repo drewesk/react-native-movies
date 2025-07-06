79724ef41a98adf36bc0717aa9f6409c5eaa3792
username <drewesk@gmail.com>
2025-07-06 12:56:16 -0400

    final formatting of Trending List

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,7 +1,7 @@
-import MaskedView from '@react-native-masked-view/masked-view';
+import { images } from "@/constants/images";
+import MaskedView from "@react-native-masked-view/masked-view";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity } from "react-native";
-
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const TrendingCard = ({
   movie: { movie_id, title, poster_url },
@@ -16,18 +16,26 @@ const TrendingCard = ({
           resizeMode="cover"
         />
 
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+          <MaskedView
+            maskElement={
+              <Text className="font-bold text-white text-6xl">{index + 1}</Text>
+            }
+          >
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
+            />
+          </MaskedView>
+        </View>
+
         <Text
           className="text-sm font-bold mt-2 text-light-200"
           numberOfLines={2}
         >
           {title}
         </Text>
-        <MaskedView
-            maskElement={
-                <Text className='font-bold text-white text-6xl'>{index + 1}</Text>    
-            }
-        ></MaskedView>
-        
       </TouchableOpacity>
     </Link>
   );
